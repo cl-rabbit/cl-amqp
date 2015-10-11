ed61d509995252c503ed00a382c3a237b7a3f16d
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-11 08:30:13 -0700

    check type of array produced by binary-string-reader

--- t/util/binary-string.lisp
+++ t/util/binary-string.lisp
@@ -5,6 +5,10 @@
 
 (amqp:enable-binary-string-syntax)
 (subtest "Testing binary string reader"
-  (is #b"\x3\x0\x3\x0\x0\x0\001a\xce" #(3 0 3 0 0 0 1 97 206) :test #'equalp))
+  (is #b"\x3\x0\x3\x0\x0\x0\001a\xce" #(3 0 3 0 0 0 1 97 206)
+      "#b\"\\x3\\x0\\x3\\x0\\x0\\x0\\001a\\xce\" expected to produce #(3 0 3 0 0 0 1 97 206)"
+      :test #'equalp)
+  (is-type #b"\x3\x0\x3\x0\x0\x0\001a\xce" '(simple-array (unsigned-byte 8))
+           "#b\"\\x3\\x0\\x3\\x0\\x0\\x0\\001a\\xce\" expected to produce (simple-array (unsigned-byte 8)"))
 
 (finalize)
