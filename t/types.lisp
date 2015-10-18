3851464ada27c619a9775e6fc594ff959a52e421
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-18 22:11:23 +0300

    test single and double decoding

--- t/types.lisp
+++ t/types.lisp
@@ -11,7 +11,7 @@
 
 (subtest "AMQP Table encode/decode test"
   (let ((amqp-table-bytes (concatenate '(simple-array  (unsigned-byte 8) 1)
-                                       #b"\x00\x00\x00\xcb"
+                                       #b"\x00\x00\x00\xe7"
                                        #b"\x05arrayA\x00\x00\x00\x0fI\x00\x00\x00\x01I\x00\x00\x00\x02I\x00\x00\x00\x03"
                                        #b"\x07boolvalt\x01"
                                        #b"\x0dboolval_falset\x00"
@@ -22,6 +22,8 @@
                                        #b"\x07longvall\x00\x00\x00\x006e&U"
                                        #b"\x04nullV"
                                        #b"\x06strvalS\x00\x00\x00\x04Test"
+                                       #b"\x06singlef\x40\x48\xf5\xc3"
+                                       #b"\x06doubled\x40\xd\x55\x55\x55\x55\x4f\x78"
                                        #b"\x0ctimestampvalT\x00\x00\x00\x00Ec)\x92"
                                        #b"\x07unicodeS\x00\x00\x00\x08utf8=\xe2\x9c\x93"))
         (table '(("array" . #(1 2 3))
@@ -34,6 +36,8 @@
                  ("longval" . 912598613)
                  ("null" . :void)
                  ("strval" . "Test")
+                 ("single" . 314)
+                 ("double" . 3.666666666666d0)
                  ("timestampval" . @2006-11-21T16:30:10)
                  ("unicode" . "utf8=✓"))))
 
