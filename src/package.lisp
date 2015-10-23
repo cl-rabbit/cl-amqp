b74411aa93a828a88af04389efb2d1499f43d63d
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-23 14:06:13 +0300

    export some symbols

--- src/package.lisp
+++ src/package.lisp
@@ -3,17 +3,25 @@
 (defpackage :cl-amqp
   (:use :cl :alexandria :collectors)
   (:nicknames :amqp)
-  (:export :error-type-from-reply-code
-           :amqp-unknown-reply-code-error
+  (:export #:error-type-from-reply-code
+           #:amqp-unknown-reply-code-error
 
-           :make-frame-parser ;; frames stuff
-           :frame-parser-consume
-           :malformed-frame-error
-           :invalid-frame-parser-state-error
-           :frame-class-from-frame-type
-           :amqp-unknown-frame-type-error
+           #:make-frame-parser ;; frames stuff
+           #:method-frame
+           #:frame-encode
+           #:frame-parser-consume
+           #:malformed-frame-error
+           #:invalid-frame-parser-state-error
+           #:frame-class-from-frame-type
+           #:amqp-unknown-frame-type-error
 
-           :enable-binary-string-syntax
-           :disable-binary-string-syntax
-           :enable-binary-string-printing
-           :disable-binary-string-printing))
+           #:enable-binary-string-syntax
+           #:disable-binary-string-syntax
+           #:enable-binary-string-printing
+           #:disable-binary-string-printing
+
+           #:new-obuffer
+           #:obuffer-get-bytes
+
+           #:amqp-method-basic-ack ;; TODO: generate for this and other methods
+   ))
