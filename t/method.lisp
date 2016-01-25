86b5e83f1aa3500dce61f709ea659eac47896ee0
Ilya Khaprov <dead.trickster@gmail.com>
2016-01-26 01:49:45 +0300

    [t] method: test methods assembler

--- t/method.lisp
+++ t/method.lisp
@@ -4,6 +4,12 @@
 
 (plan 1)
 
+(defclass test-method-assembler (method-assembler)
+  ((consume-method-lambda :initarg :cm-lambda :reader consume-method-lambda)))
+
+(defmethod consume-method ((mc test-method-assembler) method)
+  (funcall (consume-method-lambda mc) method))
+
 (subtest "Methods to/from frames"
   (subtest "Methods to frames"
     (subtest "Content method with content"
@@ -48,6 +54,88 @@
         (is (length frames) 1 "Single frame")
 
         (is-type (first frames) 'method-frame)
-        (is (frame-channel (first frames)) 2)))))
+        (is (frame-channel (first frames)) 2))))
+
+  (subtest "Frames to methods"
+    (subtest "Method without content"
+      (let* ((method (make-instance 'amqp:amqp-method-basic-ack :delivery-tag 100))
+             (frame (make-instance 'amqp:method-frame :channel 1 :payload method))
+             (cb-fired)
+             (assembler (make-instance 'test-method-assembler :cm-lambda (lambda (m)
+                                                                           (setf cb-fired t)
+                                                                           (is m method)))))
+        (consume-frame assembler frame)
+        (is (slot-value assembler 'amqp::state) :start)
+        (is cb-fired t "callback fired")))
+
+    (subtest "Content method without body"
+      (let* ((method (make-instance 'amqp:amqp-method-basic-deliver :delivery-tag 100))
+             (mframe (make-instance 'amqp:method-frame :channel 1 :payload method))
+             (basic-properties (make-instance 'amqp:amqp-basic-class-properties :delivery-mode 2))
+             (hframe (make-instance 'amqp:header-frame :channel 1 :body-size 0 :payload basic-properties))
+             (cb-fired)
+             (assembler (make-instance 'test-method-assembler :cm-lambda (lambda (m)
+                                                                           (setf cb-fired t)
+                                                                           (is m method)
+                                                                           (is (amqp-method-content-properties m) basic-properties)))))
+        (consume-frame assembler mframe)
+        (consume-frame assembler hframe)
+        (is (slot-value assembler 'amqp::state) :start)
+        (is cb-fired t "callback fired")))
+
+    (subtest "Content method with body"
+      (let* ((method (make-instance 'amqp:amqp-method-basic-deliver :delivery-tag 100))
+             (mframe (make-instance 'amqp:method-frame :channel 1 :payload method))
+             (basic-properties (make-instance 'amqp:amqp-basic-class-properties :delivery-mode 2))
+             (hframe (make-instance 'amqp:header-frame :channel 1 :body-size 12 :payload basic-properties))
+             (cb-fired)
+             (assembler (make-instance 'test-method-assembler :cm-lambda (lambda (m)
+                                                                           (setf cb-fired t)
+                                                                           (is m method)
+                                                                           (is (amqp-method-content-properties m) basic-properties)
+                                                                           (is (babel:octets-to-string (amqp-method-content m)) "Hello World!")))))
+        (consume-frame assembler mframe)
+        (is (slot-value assembler 'amqp::state) :header)
+        (consume-frame assembler hframe)
+        (is (slot-value assembler 'amqp::state) :body)
+        (consume-frame assembler (make-instance 'body-frame :size 5 :payload #b"Hello"))
+        (is (slot-value assembler 'amqp::state) :body)
+        (consume-frame assembler (make-instance 'body-frame :size 5 :payload #b" Worl"))
+        (consume-frame assembler (make-instance 'body-frame :size 2 :payload #b"d!"))
+        (is (slot-value assembler 'amqp::state) :start)
+        (is cb-fired t "callback fired")))
+
+    (subtest "Frames sequence error"
+      (let* ((method (make-instance 'amqp:amqp-method-basic-deliver :delivery-tag 100))
+             (mframe (make-instance 'amqp:method-frame :channel 1 :payload method))
+             (basic-properties (make-instance 'amqp:amqp-basic-class-properties :delivery-mode 2))
+             (hframe (make-instance 'amqp:header-frame :channel 1 :body-size 12 :payload basic-properties))
+             (cb-fired)
+             (assembler (make-instance 'test-method-assembler :cm-lambda (lambda (m)
+                                                                           (setf cb-fired t)
+                                                                           (is m method)
+                                                                           (is (amqp-method-content-properties m) basic-properties)
+                                                                           (is (babel:octets-to-string (amqp-method-content m)) "Hello World!")))))
+        (consume-frame assembler mframe)
+        (is (slot-value assembler 'amqp::state) :header)
+        (consume-frame assembler hframe)
+        (is (slot-value assembler 'amqp::state) :body)
+        (consume-frame assembler (make-instance 'body-frame :size 5 :payload #b"Hello"))
+        (is-error (consume-frame assembler mframe) 'error)
+        (is cb-fired nil "callback NOT fired")))
+
+    (subtest "Max body size exceeded"
+      (let* ((method (make-instance 'amqp:amqp-method-basic-deliver :delivery-tag 100))
+             (mframe (make-instance 'amqp:method-frame :channel 1 :payload method))
+             (basic-properties (make-instance 'amqp:amqp-basic-class-properties :delivery-mode 2))
+             (hframe (make-instance 'amqp:header-frame :channel 1 :body-size #.(* 8 1024 1024) :payload basic-properties))
+             (cb-fired)
+             (assembler (make-instance 'test-method-assembler :cm-lambda (lambda (m)
+                                                                           (setf cb-fired t)
+                                                                           (is m method)
+                                                                           (is (amqp-method-content-properties m) basic-properties)))))
+        (consume-frame assembler mframe)
+        (is-error (consume-frame assembler hframe) 'error)
+        (is cb-fired nil "callback NOT fired")))))
 
 (finalize)
